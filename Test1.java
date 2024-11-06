40eb49e066dc349a45c2daa4418f0368bba067ed
anushanosina <nosinaanusha12@gmail.com>
2024-11-06 07:55:05 -0800

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,18 @@
+
+package loops;
+
+
+public class Test1 {
+    public static void main(String[] args) {
+        int a = 50;
+        while(a<=150)
+        {
+          if(isPrime(a)){
+          
+              System.out.println(a);   
+          }
+              a++;
+          }
+    }
+        
+  
